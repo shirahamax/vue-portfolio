b97934ba1905bce6a4bb771c1ea64780402af32f
RedMacbookAir <yasutomo@aplix.co.jp>
2019-05-26 16:27:49 +0900

    [feat] リンクの画像を追加

--- vue3/src/components/NavigationList.vue
+++ vue3/src/components/NavigationList.vue
@@ -1,6 +1,8 @@
 <template>
-  <v-list dense>
+  <div class="pa-1">
 
+    <!-- アバター -->
+    <v-list dense>
       <!-- avatar -->
       <v-list-tile avatar>
         <v-list-tile-avatar>
@@ -11,17 +13,57 @@
           <v-list-tile-title>shirahama_x</v-list-tile-title>
         </v-list-tile-content>
       </v-list-tile>
+    </v-list>
 
-      <v-list-tile @click="$router.push('/')">
-        <v-list-tile-action>
-          HOME
-        </v-list-tile-action>
-      </v-list-tile>
-      <v-list-tile @click="$router.push('/')">
-        <v-list-tile-action>
-          ACCOUNT
+    <!-- 紹介文 -->
+    <div row wrap class="pa-3 text-xs-left">
+
+      <!-- イントロ -->
+      <p class="caption">
+        お越しいただきありがとうございます。
+        このサイトは <a href="https://twitter.com/shirahama_x" target="_blank">@shirahama_x </a>
+        のポートフォリオの1つで、ショッピングサイトのデモページになります。
+      </p>
+      <p class="caption">
+        Web管理画面制作/モバイル対応/<br>クラウド/多言語対応を得意としています。
+      </p>
+
+      <!-- トピック -->
+      <h4>Topic</h4>
+      <p class="caption">
+        Vue cli3:vuex/vue-router, Nuxt, <br>
+        Vuetify, BootsrapVue
+        Firebase:Hosting/Functions/Firestore, <br>
+        AWS:EC2/S3/CloudFront/Route53
+        Swift
+      </p>
+
+      <!-- リンク -->
+      <h4>Links</h4>
+      <v-layout align-center>
+        <a href="https://twitter.com/shirahama_x" target="_blank" class="mr-1">
+          <img src="../assets/img/links/twitter.png" width="30px" />
+        </a>
+        <a href="https://github.com/shirahamax/vue-portfolio" target="_blank" class="mr-2">
+          <img src="../assets/img/links/GitHub.png" width="20px" />
+        </a>
+        <a href="https://qiita.com/shirahama_x" target="_blank" class="mr-2">
+          <img src="../assets/img/links/qiita.png" width="20px" />
+        </a>
+      </v-layout>
+
+    </div>
+    <v-divider></v-divider>
+    <v-list dense>
+      <v-list-tile @click="$router.push('/comment')" class="mr-0 pr-0">
+        <v-list-tile-title class="caption">
+          このページの設計について
+        </v-list-tile-title>
+        <v-list-tile-action style="min-width:30px;">
+          <v-icon class="body-2">chevron_right</v-icon>
         </v-list-tile-action>
       </v-list-tile>
     </v-list>
 
+  </div>
 </template>
